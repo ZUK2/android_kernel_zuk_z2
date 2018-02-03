01ede66fc8974ec047836a608647bdd79c0f83aa
optimumpr <6744359+optimumpr@users.noreply.github.com>
2018-02-02 20:25:47 -0500

    OTG 1 commit by  AyushR1 https://github.com/AyushR1

--- include/linux/power_supply.h
+++ include/linux/power_supply.h
@@ -404,6 +404,8 @@ extern int power_supply_set_scope(struct power_supply *psy, int scope);
 /* For APM emulation, think legacy userspace. */
 extern struct class *power_supply_class;
 
+extern int get_usb_id_state(void);
+
 static inline bool power_supply_is_amp_property(enum power_supply_property psp)
 {
 	switch (psp) {
